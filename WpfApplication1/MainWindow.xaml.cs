3c358e1937434d09c75e6e61874c7125998c72e6
evg <evguen@rambler.ru>
2018-09-12 21:47:04 +0300

    оптимизация кода

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -27,20 +27,15 @@ namespace WpfApplication1
         public MainWindow()
         {
             InitializeComponent();
-            refreshdata();
+            SetColumn();
         }
                
-        public void refreshdata()
+        public void SetColumn()
         {           
+            dt.Columns.Add("Name", typeof(String));
+            dt.Columns.Add("Price", typeof(Decimal));
+            dt.Columns.Add("Kod", typeof(String));
 
-            SqlConnection con = new SqlConnection(@"Data Source=(LocalDb)\v11.0;Initial Catalog=tempdb;Integrated Security=True");
-            con.Open();
-            SqlCommand cmd = new SqlCommand(@"SELECT Name, Price, Kod from Stock", con);
-            SqlDataAdapter sda = new SqlDataAdapter(cmd);
-            sda.Fill(dt);
-            dt.Clear();
-            con.Close();
-               
             tbType.Text = "Продажа (открыт)";
             tbNum.Text = "00000005";                    
         }
@@ -70,57 +65,61 @@ namespace WpfApplication1
                 DataRow drr = dt.NewRow();
                 DataRow row = dt2.Rows[0];
 
-                drr["Name"]    = row.ItemArray[1].ToString();
-                drr["Price"]   = row.ItemArray[2].ToString(); 
-                drr["Kod"]     = row.ItemArray[3].ToString();
+                drr["Name"] = row.ItemArray[1].ToString();
+                drr["Price"] = row.ItemArray[2].ToString();
+                drr["Kod"] = row.ItemArray[3].ToString();
 
                 dt.Rows.Add(drr);
-                                                              
+
                 var query = from dr in dt.AsEnumerable()
-                            group dr by new { name = dr.Field<string>("Name"),price = dr.Field<decimal>("Price"), kod = dr.Field<string>("Kod") } into grp
+                            group dr by new { name = dr.Field<string>("Name"), price = dr.Field<decimal>("Price"), kod = dr.Field<string>("Kod") } into grp
                             select new
                             {
-                                Name = grp.Key.name,                              
+                                Name = grp.Key.name,
                                 Sum = grp.Sum(r => Convert.ToDecimal(r.ItemArray[1])),
-                                Count = grp.Count()                               
+                                Count = grp.Count()
                             };
 
                 dt1 = dt.Clone();
-              
+
                 foreach (var x in query)
                 {
-                    DataRow dtRow   = dt1.NewRow();
-                    dtRow["Name"]   = x.Name;
-                    dtRow["Price"]  = x.Count;
-                    dtRow["Kod"]    = x.Sum;
+                    DataRow dtRow = dt1.NewRow();
+                    dtRow["Name"] = x.Name;
+                    dtRow["Price"] = x.Count;
+                    dtRow["Kod"] = x.Sum;
                     dt1.Rows.Add(dtRow);
                 }
 
                 DataGridCassa.ItemsSource = dt1.DefaultView;
                 tbCount.Text = dt1.Rows.Count.ToString();
-                sumTotal.Text = dt.Compute("SUM(Price)","").ToString();
+                sumTotal.Text = dt.Compute("SUM(Price)", "").ToString();
 
+            }
+            else if (dt2.Rows.Count == 0)
+            {
+                tbInput.Text = "Не найдено";
             }
           
         }
 
         private void Row_MouseEnter(object sender, MouseEventArgs e)
         {            
-            if (((System.Windows.Controls.DataGridRow)(sender)).Item is System.Data.DataRowView)
+            if (((DataGridRow)(sender)).Item is DataRowView)
             {
-                gtb1.Text = ((System.Data.DataRowView)(((DataGridRow)sender).Item)).Row.ItemArray[1].ToString(); //kod              
-                
-                decimal col  = Convert.ToDecimal(((System.Data.DataRowView)(((DataGridRow)sender).Item)).Row.ItemArray[1]); //кол-во             
-                decimal col1 =  Convert.ToDecimal(string.Format("{0:n3}", col));
+                gtb1.Text = ((DataRowView)(((DataGridRow)sender).Item)).Row.ItemArray[1].ToString(); //kod              
+
+                decimal col = Convert.ToDecimal(((DataRowView)(((DataGridRow)sender).Item)).Row.ItemArray[1]); //кол-во             
+                decimal col1 = Convert.ToDecimal(string.Format("{0:n3}", col));
 
                 gtb3.Text = col1.ToString();
-                
-                gtb4.Text = ((System.Data.DataRowView)(((DataGridRow)sender).Item)).Row.ItemArray[2].ToString(); //сумма
-                gtb5.Text = ((System.Data.DataRowView)(((DataGridRow)sender).Item)).Row.ItemArray[0].ToString();
-              
+
+                gtb4.Text = ((DataRowView)(((DataGridRow)sender).Item)).Row.ItemArray[2].ToString(); //сумма
+                gtb5.Text = ((DataRowView)(((DataGridRow)sender).Item)).Row.ItemArray[0].ToString();
+
                 decimal sum = Convert.ToDecimal(gtb4.Text);
 
-                gtb2.Text = (sum/col).ToString() ; //цена                             
+                gtb2.Text = (sum / col).ToString(); //цена                             
 
             }
         }              
@@ -130,5 +129,6 @@ namespace WpfApplication1
             Enter();
         }
 
+       
     }
 }
